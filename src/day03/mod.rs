7b755233abb4b8c33d7411c515d62fbb6d4eedc2
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-12-05 17:28:13 -0600

    feat: day three part two

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -4,6 +4,16 @@ pub fn main() {
     let mut total = 0;
 
     let input = fs::read_to_string("src/day03/input.txt").expect("could not read input file");
+
+    let dos = input
+        .match_indices("do()")
+        .map(|(i, _)| i)
+        .collect::<Vec<_>>();
+    let donts = input
+        .match_indices("don't()")
+        .map(|(i, _)| i)
+        .collect::<Vec<_>>();
+
     input.match_indices("mul(").for_each(|(start_index, _)| {
         let remaining = &input[start_index..];
         let instruction = match remaining.find(")") {
@@ -20,6 +30,19 @@ pub fn main() {
         if !instruction_part_is_valid.all(|f| f == true) {
             return;
         };
+
+        let operation_enabled = match donts.iter().filter(|&&i| i < start_index).max() {
+            Some(prev_dont) => match dos.iter().filter(|&&i| i < start_index).max() {
+                Some(prev_do) => prev_do > prev_dont,
+                None => false,
+            },
+            None => true,
+        };
+
+        if !operation_enabled {
+            return;
+        };
+
         let numbers = parenthesis_content
             .split(",")
             .map(|number_string| {
