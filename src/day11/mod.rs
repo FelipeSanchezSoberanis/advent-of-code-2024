54dbc0af96aa158a7bb8a05877f9b9de5c9cbb16
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2025-04-01 16:45:31 -0600

    feat: day 11 part two

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs, u128};
+use std::{collections::HashMap, fs, u128};
 
 fn parse_input(filepath: &str) -> Vec<u128> {
     fs::read_to_string(filepath)
@@ -12,36 +12,55 @@ fn parse_input(filepath: &str) -> Vec<u128> {
         .collect::<Vec<_>>()
 }
 
-fn part_one(filepath: &str) -> usize {
-    let mut stones = parse_input(filepath);
-
-    for _ in 0..25 {
-        let mut updated_stones = Vec::<u128>::new();
-
-        stones.iter().for_each(|stone| {
-            if *stone == 0 {
-                updated_stones.push(1);
-            } else if stone.to_string().len() % 2 == 0 {
-                let number_string = stone.to_string();
-                updated_stones.push(
-                    number_string[0..number_string.len() / 2]
-                        .parse()
-                        .expect("could not parse number string"),
-                );
-                updated_stones.push(
-                    number_string[number_string.len() / 2..number_string.len()]
-                        .parse()
-                        .expect("could not parse number string"),
-                );
-            } else {
-                updated_stones.push(stone * 2024);
-            }
-        });
-
-        stones = updated_stones;
+fn solve(stone: u128, blinks: usize, memory: &mut HashMap<(u128, usize), usize>) -> usize {
+    if blinks == 0 {
+        return 1;
+    } else if let Some(value) = memory.get(&(stone, blinks)) {
+        return *value;
+    } else if stone == 0 {
+        let value = solve(1, blinks - 1, memory);
+        memory.insert((stone, blinks), value);
+        return value;
+    } else if stone.to_string().len() % 2 == 0 {
+        let stone_string = stone.to_string();
+        let value = solve(
+            stone_string[0..stone_string.len() / 2]
+                .parse()
+                .expect("could not parse stone string"),
+            blinks - 1,
+            memory,
+        ) + solve(
+            stone_string[stone_string.len() / 2..stone_string.len()]
+                .parse()
+                .expect("could not parse stone string"),
+            blinks - 1,
+            memory,
+        );
+        memory.insert((stone, blinks), value);
+        return value;
+    } else {
+        let value = solve(stone * 2024, blinks - 1, memory);
+        memory.insert((stone, blinks), value);
+        return value;
     }
+}
 
-    stones.len()
+fn part_one(filepath: &str) -> usize {
+    let stones = parse_input(filepath);
+    let mut memory = HashMap::new();
+    stones
+        .iter()
+        .map(|stone| solve(*stone, 25, &mut memory))
+        .sum()
+}
+
+fn part_two(filepath: &str) -> usize {
+    let stones = parse_input(filepath);
+    let mut memory = HashMap::new();
+    stones
+        .iter()
+        .map(|stone| solve(*stone, 75, &mut memory))
+        .sum()
 }
 
 #[cfg(test)]
@@ -59,6 +78,13 @@ mod tests {
     fn test_part_one() {
         let expected = 185894;
         let actual = part_one("./src/day11/input.txt");
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let expected = 221632504974231;
+        let actual = part_two("./src/day11/input.txt");
         assert_eq!(expected, actual);
     }
 }
